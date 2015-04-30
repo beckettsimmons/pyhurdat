c646c8701921838540725f14fc85bf9366652da5
Beckett Simmons <beckettsimmons@hotmail.com>
2015-04-30 19:26:27 -0300

    Added a few useful storm methods.

--- phd/storm.py
+++ phd/storm.py
@@ -11,5 +11,24 @@ def maximum_wind_speed(storm):
     for data_line in storm['data']:
         if data_line["maximum_sustained_wind"] > max_speed:
             max_speed = data_line["maximum_sustained_wind"]
-
     return max_speed
+
+
+def made_landfall(storm):
+    """ Returns bool of whether of not the storm made landfall at any point. """
+    for data_line in storm["data"]:
+        if 'L' in data_line["identifier"]:
+            return True
+    return False
+
+
+def name(storm):
+    return storm['header']['name']
+
+
+def has_name(storm):
+    return "UNNAMED" == name(storm)
+
+
+def year(storm):
+    return storm['header']['year']
